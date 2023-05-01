a0e7b00ced646c6758c69f76dc5bcbe4f1fdb1ca
Andrew Galle <Andrew.Galle@LIVE.COM>
2023-05-01 16:25:11 -0400

    add basic logic and computer flipping chosen image based on Math.random() and Math.floor()

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
 const choiceRock = document.querySelector("#rockChosen");
 const choicePaper = document.querySelector("#paperChosen");
 const choiceScissors = document.querySelector("#scissorsChosen");
+const computerPlayImg = document.querySelector("#computerChoice");
+
 
 let playerChoice = "Select";
 
-choiceRock.addEventListener('click', (e) => console.log(e));
+function choiceMade(e) {
+    playerChoice = e.target.alt;
 
-choicePaper.addEventListener('click', (e) => console.log(e));
+    const computerPlay = Math.floor(Math.random() * 3 - 1 + 1); //Choose a number between 1 and 3
+    
+    if (computerPlay == 0) {
+        computerPlayImg.src = "images/rock90x90.png";
+    } else if (computerPlay == 1) {
+        computerPlayImg.src = "images/paper90x90.png";
+    } else {
+        computerPlayImg.src = "images/scissors90x90.png";
+    }
+    
 
-choiceScissors.addEventListener('click', (e) => console.log(e));
+}
+
+/* Set up an event listener for each picture the player could chose. Then, take the "alt" tag on the image and replace that for the player choice. So, if they click the image of the rock, player choice become rock*/
+choiceRock.addEventListener('click', choiceMade);
+
+choicePaper.addEventListener('click', choiceMade);
+
+choiceScissors.addEventListener('click', choiceMade);
+
+
+
 
 
 
