12f75846195d360a42d1bb0a87c4e4232b5a7488
Andrew Galle <Andrew.Galle@LIVE.COM>
2023-05-01 15:46:23 -0400

    Add initial layout and images

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rockPaperScissors
+
+This is an Odin Project assignment to create a "Rock, paper, scissors" game with plain javascript.  I added a GUI.
---
+++ js/script.js
@@ -0,0 +1,14 @@
+const choiceRock = document.querySelector("#rockChosen");
+const choicePaper = document.querySelector("#paperChosen");
+const choiceScissors = document.querySelector("#scissorsChosen");
+
+let playerChoice = "Select";
+
+choiceRock.addEventListener('click', (e) => console.log(e));
+
+choicePaper.addEventListener('click', (e) => console.log(e));
+
+choiceScissors.addEventListener('click', (e) => console.log(e));
+
+
+
